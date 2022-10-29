88e7b92fbd2272983ef470e5d59416e3926a6e79
khoabd <khoabd98@gmail.com>
2022-10-29 10:40:13 +0700

    [UPDATE] decimal for point = 10**18

--- schedules/cron_staking.py
+++ schedules/cron_staking.py
@@ -21,7 +21,7 @@ from lib import dt_utcnow
 from tasks import on_update_staking_rank
 
 SLEEP_TIME = 10  # time get data
-POINT_DECIMALS = 10**6
+POINT_DECIMALS = 10**18
 
 db = MongoClient(Config.MONGO_URI, connect=False)['katana-dapp']
 
