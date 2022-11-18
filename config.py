817a2945b682b7ae2e0668e3772a7546323142dc
phongphong <uithuyphong>
2022-11-18 10:11:04 +0700

    [Box]: update queue

--- config.py
+++ config.py
@@ -32,9 +32,9 @@ class Config():
         'worker.send_referral_reward': {'queue': 'ktn-jobs-queue'},
         'worker.on_mint_order_from_dev': {'queue': 'ktn-referral-commission-queue'},
         'worker.on_mint_order_from_dapp_creator': {'queue': 'ktn-referral-commission-queue'},
-        'worker.on_created_box': {'ktn-nft-queue'},
-        'worker.on_transfer_box': {'ktn-nft-queue'},
-        'worker.on_open_box': {'ktn-nft-queue'},
+        'worker.on_created_box': {'queue': 'ktn-nft-queue'},
+        'worker.on_transfer_box': {'queue': 'ktn-nft-queue'},
+        'worker.on_open_box': {'queue': 'ktn-nft-queue'},
 
     }
 
