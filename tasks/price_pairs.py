1a068cba2ea635d8ae6581371926af20aabcbb6d
theanh <theanhvo221@gmail.com>
2023-03-06 17:49:32 +0700

    add cron price from coingecko

--- tasks/price_pairs.py
+++ tasks/price_pairs.py
@@ -34,18 +34,31 @@ def on_save_price(price_data, pairs):
             
         if dex_name == 'Gemini':
             _prices = price_response.get("prices")   
-
-        return _prices 
-    
+     
     def get_price_pairs_key(symbol):
         return f'katana-dapp.price_pairs/{symbol}'
 
+    def get_price_cmc_key(symbol):
+        return f'katana-dapp.price_cmc/{symbol}'
+    
     try:
         if not pairs:
             return 'FAIL - pair not found'
 
         _dex_name = py_.get(price_data, 'name')
         _price_response = py_.get(price_data, 'price_response')
+        if _dex_name == "Coingecko":
+            _info_price = {
+                "source": _dex_name,
+                "url_source": py_.get(price_data, 'url'),
+                "symbol": py_.get(_price_response, 'symbol'),
+                "price": py_.get(_price_response, 'current_price'),
+                "updated_time": dt_utcnow().timestamp()
+            }
+            _key = get_price_cmc_key(symbol=py_.get(_price_response, 'symbol'))
+            redis_cluster.set(_key, json.dumps(_info_price))
+            return "DONE - on_save_price CMC"
+        
         _prices = filter_price_by_dex(dex_name=_dex_name, price_response=_price_response)
         if not _prices:
             sentry_sdk.capture_message(f'on_save_price not found price data')
