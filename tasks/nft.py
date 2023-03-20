f7c902b4a81224aa848cfac12ea25f067bc087ca
Pham Anh Tai <phamanhtai0410@gmail.com>
2023-03-20 14:02:18 +0700

    check null ImageUrl and AnimationModelUrl

--- tasks/nft.py
+++ tasks/nft.py
@@ -319,8 +319,10 @@ def on_mint_from_box(self, _event):
             # pop the unnecessary infos in `attributes`
             _chosen_type.pop("rate")
             _chosen_type.pop("price")
-            _chosen_type.pop("ImageUrl")
-            _chosen_type.pop("AnimationModelUrl")
+            if py_.get(_chosen_type, "ImageUrl"):
+                _chosen_type.pop("ImageUrl")
+            if py_.get("AnimationModelUrl"):
+                _chosen_type.pop("AnimationModelUrl")
             _chosen_type.pop("AssetDescription")
 
             # structure the `metadata` object for message
